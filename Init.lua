a6228cedbcdc6a066cccb24f0fbd73d1707ce9e1
David C. Uhrig <david@twintop-tahoe.com>
2020-11-09 18:17:38 +1100

    Rev

--- Init.lua
+++ Init.lua
@@ -2,7 +2,7 @@ local _, TRB = ...
 
 -- Addon details data
 TRB.Details = {}
-TRB.Details.addonVersion = "9.0.2.19"
+TRB.Details.addonVersion = "9.0.2.20"
 TRB.Details.addonReleaseDate = "November 09, 2020"
 TRB.Details.supportedSpecs = "Shadow Priest, Elemental Shaman, Balance Druid"
 
