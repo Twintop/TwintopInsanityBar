1099bf4c1c61db1b6f369ecfc1e66552d81c04fc
David C. Uhrig <david@twintop-tahoe.com>
2021-05-20 00:42:15 +1000

    Rev

--- Init.lua
+++ Init.lua
@@ -6,8 +6,8 @@ TRB.Details.addonVersion = GetAddOnMetadata(addonName, "Version")
 TRB.Details.addonAuthor = GetAddOnMetadata(addonName, "Author")
 TRB.Details.addonAuthorServer = GetAddOnMetadata(addonName, "AuthorServer")
 TRB.Details.addonTitle = GetAddOnMetadata(addonName, "Title")
-TRB.Details.addonReleaseDate = "April 29, 2021"
-TRB.Details.supportedSpecs = "|cFFFF7C0ADruid|r - Balance\n|cFFAAD372Hunter|r - Beast Mastery, Marksmanship, Survival\n|cFFFFFFFFPriest|r - Shadow\n|cFF0070DDShaman|r - Elemental\n|cFFC69B6DWarrior|r - Arms"
+TRB.Details.addonReleaseDate = "May 20, 2021"
+TRB.Details.supportedSpecs = "|cFFFF7C0ADruid|r - Balance\n|cFFAAD372Hunter|r - Beast Mastery, Marksmanship, Survival\n|cFFFFFFFFPriest|r - Holy, Shadow\n|cFF0070DDShaman|r - Elemental\n|cFFC69B6DWarrior|r - Arms"
 
 local addonData = {
 	loaded = false,
