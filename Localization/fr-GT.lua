9df4ecd6130963a1c1644c73619a086eeaac5366
David C. Uhrig <david@twintop-tahoe.com>
2024-11-09 11:13:02 +1100

    Update fr-GT.lua
    Move to end of file.

--- Localization/fr-GT.lua
+++ Localization/fr-GT.lua
@@ -1310,8 +1310,6 @@ if locale == "frFR" then
     L["DruidBalanceBarTextVariable_starweaver"] = "Le processus Chaîne/Trame de Starweaver est actif. Variable logique uniquement !"
     L["DruidBalanceBarTextVariable_starweaversWarp"] = "Le processus Warp de Starweaver est actif. Variable logique uniquement !"
     L["DruidBalanceBarTextVariable_starweaversWeft"] = "Le processus Trame de Starweaver est actif. Variable logique uniquement !"
-    L["DruidBalanceBarTextVariable_starlordTime"] = "Temps restant de Seigneur des étoiles"
-    L["DruidBalanceBarTextVariable_starlordStacks"] = "Nombre de cumul du buff Seigneur des étoiles"
     L["DruidFeralBarTextVariable_energy"] = "Énergie actuelle"
     L["DruidFeralBarTextVariable_resource"] = "Énergie actuelle"
     L["DruidFeralBarTextVariable_energyMax"] = "Énergie maximale"
@@ -2052,4 +2050,6 @@ if locale == "frFR" then
     L["PaladinHolyBarTextVariable_channeledMana"] = "Mana pendant la canalisation du Sérum d'âme endormie"
     L["PaladinHolyBarTextVariable_slumberingSoulSerumTicks"] = "Nombre de ticks restants pour canaliser le Sérum d'âme endormie"
     L["PaladinHolyBarTextVariable_slumberingSoulSerumTime"] = "Temps restant, en secondes, pour canaliser le Sérum d'âme endormie"
+    L["DruidBalanceBarTextVariable_starlordTime"] = "Temps restant de Seigneur des étoiles"
+    L["DruidBalanceBarTextVariable_starlordStacks"] = "Nombre de cumul du buff Seigneur des étoiles"
 end
