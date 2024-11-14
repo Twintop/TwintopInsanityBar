6ea75f0c4263a1e8ac3f17e7f8d30651636a2bf8
David C. Uhrig <david@twintop-tahoe.com>
2024-11-15 00:54:57 +1100

    Fix Holy Paladin mana cost for casted spells

--- ClassModules/Paladin.lua
+++ ClassModules/Paladin.lua
@@ -637,16 +637,16 @@ local function CastingSpell()
 				TRB.Functions.Character:ResetCastingSnapshotData()
 				return false
 			else
-				local _, _, spellIcon, _, _, _, spellId = C_Spell.GetSpellInfo(currentSpellName)
+				local spellInfo = C_Spell.GetSpellInfo(currentSpellName) --[[@as SpellInfo]]
 
-				if spellId then
-					local manaCost = -TRB.Classes.SpellBase.GetPrimaryResourceCost({ id = spellId, primaryResourceType = Enum.PowerType.Mana, primaryResourceTypeProperty = "cost", primaryResourceTypeMod = 1.0 }, true)
+				if spellInfo ~= nil and spellInfo.spellID then
+					local manaCost = -TRB.Classes.SpellBase.GetPrimaryResourceCost({ id = spellInfo.spellID, primaryResourceType = Enum.PowerType.Mana, primaryResourceTypeProperty = "cost", primaryResourceTypeMod = 1.0 }, true)
 
-					casting.startTime = currentSpellStartTime / 1000
-					casting.endTime = currentSpellEndTime / 1000
-					casting.resourceRaw = manaCost
-					casting.spellId = spellId
-					casting.icon = string.format("|T%s:0|t", spellIcon)
+					snapshotData.casting.startTime = currentSpellStartTime / 1000
+					snapshotData.casting.endTime = currentSpellEndTime / 1000
+					snapshotData.casting.resourceRaw = manaCost
+					snapshotData.casting.spellId = spellInfo.spellID
+					snapshotData.casting.icon = string.format("|T%s:0|t", spellInfo.iconID)
 
 					UpdateCastingResourceFinal_Holy()
 				else
