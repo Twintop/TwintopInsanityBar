cf65079c06c0cffc42fda550f736228ea1e0f30a
David C. Uhrig <david@twintop-tahoe.com>
2025-03-10 12:50:22 +1100

    More color picker fixes

--- Options/OptionsUi.lua
+++ Options/OptionsUi.lua
@@ -246,13 +246,13 @@ function TRB.Functions.OptionsUi:ColorOnMouseDown(button, colorTable, colorContr
 		
 			if frame ~= nil then
 				if frameType == "backdrop" then
-					TRB.Functions.Color:SetBackdropColor(frame, colorTable[key].color, true, specId)
+					TRB.Functions.Color:SetBackdropColor(frame, nil, r_1, g_1, b_1, a_1)
 				elseif frameType == "border" then
 					TRB.Functions.Color:SetBackdropBorderColorFromRGBAString(frame, nil, colorTable[key].color)
 				elseif frameType == "bar" then
 					TRB.Functions.Color:SetStatusBarColorFromRGBAString(frame, nil, colorTable[key].color)
 				elseif frameType == "threshold" then
-					TRB.Functions.Color:SetThresholdColor(frame, colorTable[key].color, true, specId)
+					TRB.Functions.Color:SetThresholdColor(frame, nil, colorTable[key].color, true, specId)
 				end
 			end
 		end)
@@ -275,7 +275,7 @@ function TRB.Functions.OptionsUi:ColorOnMouseDown_OLD(button, colorTable, colorC
 				elseif frameType == "bar" then
 					TRB.Functions.Color:SetStatusBarColorFromRGBAString(frame, nil, colorTable[key])
 				elseif frameType == "threshold" then
-					TRB.Functions.Color:SetThresholdColor(frame, nil, colorTable[key].color, true, specId)
+					TRB.Functions.Color:SetThresholdColor(frame, nil, colorTable[key], true, specId)
 				end
 			end
 		end)
