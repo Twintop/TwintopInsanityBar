67be237434e73da0c5a1fc277b55cc972fe9f3aa
David C. Uhrig <david@twintop-tahoe.com>
2026-02-21 22:40:42 +1100

    Clean up missed string references to Devouring Plague

--- Localization/enGB.lua
+++ Localization/enGB.lua
@@ -98,7 +98,7 @@ if locale == "enGB" then
     L["PriestShadowCheckboxMindFlayInsanityTooltip"] = "This will change the bar border colour when you are able to cast Mind Flay: Insanity"
     L["PriestShadowCheckboxMindDevourerTooltip"] = "This will change the bar border colour when you are able to cast Shadow Word: Madness for 0 Insanity cost via a Mind Devourer proc."
     L["PriestShadowTextColorsHeader"] = "Insanity Text Colours"
-    L["PriestShadowCheckboxThresholdOverTooltip"] = "This will change the Insanity text colour when you are able to cast Devouring Plague"
+    L["PriestShadowCheckboxThresholdOverTooltip"] = "This will change the Insanity text colour when you are able to cast Shadow Word: Madness"
     L["ShamanManaCheckboxAscendanceEnd"] = "Ascendance colour change when ending enabled"
     L["ShamanManaCheckboxAscendanceEndTooltip"] = "Changes the bar colour when Ascendance is ending in the next X GCDs or fixed length of time. Select which to use from the options below."
     L["ShamanElementalCheckboxAscendanceEnd"] = "Ascendance colour change when ending enabled"
