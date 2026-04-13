fb0363fdb1095dec8c67009806ef15c89849dc19
M.O.S.S <117463487+MOSS099@users.noreply.github.com>
2026-04-13 10:30:18 +0800

    Add zhCN localization for Divine Purpose strings

--- Localization/zhCN.lua
+++ Localization/zhCN.lua
@@ -2407,4 +2407,13 @@ if locale == "zhCN" then
     L["GradientDirectionHorizontalAbbrev"] = "水"
     L["GradientDirectionVerticalAbbrev"] = "垂"
 
+	L["PaladinBarTextVariable_divinePurposeActive"] = "神圣意志当前是否激活？仅用于逻辑变量！"
+	L["PaladinIndicatorDivinePurpose"] = "神圣意志"
+	L["PaladinIndicatorDivinePurposeTooltip"] = "获得神圣意志效果时，指示器将切换为此颜色。"
+	L["PaladinIndicatorDivinePurposeColor"] = "神圣意志已激活"
+	
+	L["PaladinAudioDivinePurpose"] = "神圣意志"
+	L["PaladinAudioCheckboxDivinePurpose"] = "神圣意志触发时播放音效"
+	L["PaladinAudioCheckboxDivinePurposeTooltip"] = "神圣意志触发时播放音效，每次触发仅播放一次。"
+
 end
