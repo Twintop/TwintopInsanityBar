c03c2f581327ac96e23cd8a0145cebebbd7a3ff8
Twintop <david@twintop-tahoe.com>
2026-07-03 14:43:20 +1000

    News

--- Functions/News.lua
+++ Functions/News.lua
@@ -16,9 +16,8 @@ local content = [====[
 ## Druid
 ### Feral
 
-- Add support for detecting the Midnight Season 2 set bonus.
-- 4-Piece: Account for the additional 10 seconds of duration granted to Berserk / Incarnation: Avatar of Ashamane.
 - 2-Piece: Add tracking for the Halazzi's Fury damage buff gained when Berserk / Incarnation: Avatar of Ashamane ends (1 second per Combo Point spent during it), shown as a new Color Indicator. Adds a bar text timer variable `$halazzisFuryTime` and an icon (`#halazzisFury`).
+- 4-Piece: Account for the additional 10 seconds of duration granted to Berserk / Incarnation: Avatar of Ashamane.
 
 ## Hunter
 ### Beast Mastery
