ab61a6afa15a2c20e932aa69ab91d6ee09c2ed16
Twintop <david@twintop-tahoe.com>
2026-08-25 20:47:04 +1000

    Fix news and TOC.

--- Functions/News.lua
+++ Functions/News.lua
@@ -12,11 +12,15 @@ local content = [====[
 
 ---
 
-# 12.1.0.6-release (2026-08-24)
+# 12.1.0.7-release (2026-08-26)
 ## General
 
 - Improve bar update performance, primarily around threshold lines and bar text.
 
+---
+
+# 12.1.0.6-release (2026-08-24)
+## General
 ### Cast Bars
 
 - Fix bar text blanking the moment a cast ended, leaving the Player, Target and Focus cast bars on screen with no text while they faded out. The bar text when the cast finishes now persists until the bar is gone.
